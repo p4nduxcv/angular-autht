f1f552db81b3508047bf5d64faa2a69b91fd6f75
pandula <pandula95@gmail.com>
2021-05-26 13:58:16 +0530

    Mongo DB Connections

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const db =
+  "mongodb+srv://panduxcv:qtek9100@parkme.lolik.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(db, (err) => {
+  if (err) {
+    console.error(` Error`, err);
+  } else {
+    console.log("Mongo Elakiri wage wada");
+  }
+});
 
 router.get("/", (req, res) => {
   res.send("From API Service");
