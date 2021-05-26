21e2e49b4469d4c3be090e93c199fe32c7e84caa
pandula <pandula95@gmail.com>
2021-05-26 17:34:50 +0530

    Login API

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const User = require("../models/user");
 
 const db =
-  "mongodb+srv://panduxcv:qtek9100@parkme.lolik.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://panduxcv:qtek9100@parkme.lolik.gcp.mongodb.net/eventsdb?retryWrites=true&w=majority";
 
 mongoose.connect(db, (err) => {
   if (err) {
@@ -22,12 +22,30 @@ router.get("/", (req, res) => {
 
 router.post("/register", (req, res) => {
   let userData = req.body;
-  let user = new User(userData);
-  user.save(err, (registereduser) => {
+  let users = new User(userData);
+  users.save((error, registereduser) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(200).send(registereduser);
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  let userData = req.body;
+
+  User.findOne({ email: userData.email }, (err, user) => {
     if (err) {
       console.log(err);
     } else {
-      res.status(200).send(registereduser);
+      if (!user) {
+        res.status(401).send("Invalid-Email");
+      } else if (user.password !== userData.password) {
+        res.status(401).send("Invalid-Password");
+      } else {
+        res.status(200).send(user);
+      }
     }
   });
 });
